579ea392b1208758bd2399813530f0b2105993ac
Sean Xavier <seanxhallion@gmail.com>
2016-12-13 18:39:58 -0500

    variables with strings, numbers, booleans, and arrays with a for loop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 // create a variable with a string stored inside
 var neonGenesis = 'Evangelion'; 
+
 // create a variable with a number stored inside
 var evaUnit = 01;
+
 // create a variable with a boolean stored inside
 var readyLaunch = true;
+
 // create a variable with an array stored inside
 var pilots = ['Asuka', 'Shinji', 'Rei']
+
 // create a for loop that loops over your array
 // logging each item to the console
 for(var i = 0; i < pilots.length; i++){
